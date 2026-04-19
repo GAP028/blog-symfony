576a970e4e80157f5a796e4f19b839fe35539718
Gomas <gomasalainpatrick8@gmail.com>
2026-04-19 13:25:53 +0200

    Amelioration des formulaires d inscription et connexion en francais avec meilleur design

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,11 +19,33 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('profilePicture')
+            ->add('firstName', null, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom',
+                ],
+            ])
+            ->add('lastName', null, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom',
+                ],
+            ])
+            ->add('email', null, [
+                'label' => 'Adresse e-mail',
+                'attr' => [
+                    'placeholder' => 'exemple@mail.com',
+                ],
+            ])
+            ->add('profilePicture', UrlType::class, [
+                'label' => 'Photo de profil (URL)',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'https://exemple.com/photo.jpg',
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'J’accepte les conditions d’utilisation',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue(
@@ -34,8 +57,18 @@ class RegistrationFormType extends AbstractType
                 'type' => PasswordType::class,
                 'mapped' => false,
                 'invalid_message' => 'Les mots de passe doivent être identiques.',
-                'first_options' => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Choisissez un mot de passe',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmez le mot de passe',
+                    ],
+                ],
                 'constraints' => [
                     new NotBlank(
                         message: 'Veuillez saisir un mot de passe'
